26033bdb1b3cc575eaa4ed2c63f1d34beefc0d9f
priyanka19697 <priyanka19697@gmail.com>
2021-04-18 00:43:33 +0530

    day3

--- tweets/models.py
+++ tweets/models.py
@@ -6,9 +6,13 @@ import random
 class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+
     def serialize(self):
         return{
-            "id":self.id,
-            "content":self.content,
-            "likes": random.randint(0,200)
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
         }
